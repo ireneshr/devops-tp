314146817431a945f94991e53f9aad9d60384b9a
Irene Hermosilla <ireneshr.1994@gmail.com>
2023-10-17 21:55:53 -0300

    Endpoints

---
+++ src/db.py
@@ -0,0 +1,27 @@
+empDB = [
+  {
+    "id": "0",
+    "name": "Josephine Hawkins",
+    "phone": "+1 (862) 428-2160"
+  },
+  {
+    "id": "1",
+    "name": "Barrett Fulton",
+    "phone": "+1 (881) 496-2875"
+  },
+  {
+    "id": "2",
+    "name": "Erin Lynch",
+    "phone": "+1 (827) 524-3459"
+  },
+  {
+    "id": "3",
+    "name": "Valerie Potter",
+    "phone": "+1 (965) 586-2319"
+  },
+  {
+    "id": "4",
+    "name": "Brandi Douglas",
+    "phone": "+1 (817) 422-3504"
+  }
+]
---
+++ src/routes.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request, abort
+from db import empDB
+
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route('/employee', methods=['GET'])
+def getAllEmp():
+    return jsonify(empDB)
+
+
+@app.route('/employee/<empId>', methods=['GET'])
+def getEmp(empId):
+    usr = [ emp for emp in empDB if (emp['id'] == empId) ]
+    return jsonify(usr)
+
+
+@app.route('/employee/<empId>', methods=['PUT'])
+def updateEmp(empId):
+    em = [ emp for emp in empDB if (emp['id'] == empId) ]
+    if 'name' in request.json :
+        em[0]['name'] = request.json['name']
+    if 'phone' in request.json:
+        em[0]['phone'] = request.json['phone']
+    return jsonify(em[0])
+
+
+@app.route('/employee', methods=['POST'])
+def createEmp():
+    dat = {
+    'id':request.json['id'],
+    'name':request.json['name'],
+    'phone':request.json['phone']
+    }
+    empDB.append(dat)
+    return jsonify(dat)
+
+
+@app.route('/employee/<empId>', methods=['DELETE'])
+def deleteEmp(empId):
+    em = [ emp for emp in empDB if (emp['id'] == empId) ]
+    if len(em) == 0:
+        abort(404)
+    empDB.remove(em[0])
+    return jsonify({'response':'Success'})
